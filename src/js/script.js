e453a8e163f9cbd079bc9994827a2a3831ad7f35
Vladimir <vl.podgorny@gmail.com>
2024-06-18 23:46:20 +0200

    first slide

---
+++ src/js/script.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1100,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.png"></button>',
+        responsive: [
+            {
+                breakpoint: 991,
+                settings: {
+                    dots: true,
+                    arrows: false,
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 767,
+                settings: {
+                    dots: true,
+                    arrows: false,
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 575,
+                settings: {
+                    dots: true,
+                    arrows: false,
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+});
+      
