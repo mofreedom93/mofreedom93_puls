51d0153298433609d47eb1ead18e3239909ddc2d
Vladimir <vl.podgorny@gmail.com>
2024-06-27 19:23:03 +0200

    scrollup

--- src/js/script.js
+++ src/js/script.js
@@ -126,5 +126,21 @@ $(document).ready(function(){
         });
         return false;
     });
+
+    //UP SCROLL
+
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+
+    $("a[href^='#']").click(function(){
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
 });
 
