85192f9e04d2946f9c9d199442ba0ae6b137948b
Vladimir <vl.podgorny@gmail.com>
2024-06-27 18:32:52 +0200

    smart email

--- src/js/script.js
+++ src/js/script.js
@@ -107,5 +107,24 @@ $(document).ready(function(){
 
     
     $('input[name=phone]').mask("+7 (999) 999-99-99");
+
+    $('form').submit(function(e) {
+        e.preventDefault();
+        if(!$(this).valid()) {
+            return;
+        }
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+
+            $('form').trigger('reset');
+        });
+        return false;
+    });
 });
 
